49312d83c632dd69d0049859a3b3ce7108018c36
kite-torder <jjy5@torder.com>
2024-03-08 12:00:49 +0900

    feat: 슬라이더 위젯과 동영상 연동하기

--- lib/component/custom_video_player.dart
+++ lib/component/custom_video_player.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:video_player_kite/constant/styles.dart';
+import 'package:video_player/video_player.dart';
+import 'dart:io';
 
 // 동영상 위젯 생성
 class CustomVideoPlayer extends StatefulWidget {
@@ -18,13 +20,65 @@ class CustomVideoPlayer extends StatefulWidget {
 }
 
 class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  // 동영상을 조작하는 컨트롤러
+  VideoPlayerController? videoPlayerController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    initializeController(); // 컨트롤러 초기화
+  }
+
+  initializeController() async {
+    // 선택한 동영상으로 컨트롤러 초기화
+    final videoController = VideoPlayerController.file(
+      File(widget.video.path),
+    );
+
+    await videoController.initialize();
+
+    setState(() {
+      this.videoPlayerController = videoController;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'CustomVideoPlayer',
-        style: AppTextStyles.esamanru_30_w300,
-      ),
-    );
+    // 동영상 컨트롤러가 준비 중일 때 로딩 표시
+    if (videoPlayerController == null) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    return AspectRatio(
+        aspectRatio: videoPlayerController!.value.aspectRatio,
+        child: Stack(
+          children: [
+            VideoPlayer(
+              videoPlayerController!,
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: Slider(
+                // 슬라이더가 이동할 때마다 실행할 함수
+                onChanged: (double val) {
+                  videoPlayerController!.seekTo(
+                    Duration(seconds: val.toInt()),
+                  );
+                },
+
+                // 동영상 재생 위치를 초 단위로 구현
+                value:
+                    videoPlayerController!.value.position.inSeconds.toDouble(),
+                min: 0,
+                max: videoPlayerController!.value.duration.inSeconds.toDouble(),
+              ),
+            )
+          ],
+        ));
   }
 }
