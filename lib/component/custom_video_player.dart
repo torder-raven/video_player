a8b30dc4253364a37a1dd6176b0b7b7f94c292c9
kite-torder <jjy5@torder.com>
2024-03-08 12:21:44 +0900

    feat: 동영상 컨트롤러 기능 연결

--- lib/component/custom_video_player.dart
+++ lib/component/custom_video_player.dart
@@ -1,6 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:video_player_kite/constant/styles.dart';
 import 'package:video_player_kite/component/custom_icon_button.dart';
 import 'package:video_player/video_player.dart';
 import 'dart:io';
@@ -39,9 +40,27 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
 
     await videoController.initialize();
 
+    // 컨트롤러의 속성이 변경될 때마다 실행할 함수 등록
+    videoController.addListener(videoControllerListener);
+
     setState(() {
       this.videoPlayerController = videoController;
     });
+  }
+
+  // 동영상의 재생 상태가 변경될 때마다
+  // setState()를 실행해서 build()를 재실행합니다.
+  void videoControllerListener() {
+    setState(() {
+
+    });
+  }
+
+  // State가 폐기될 때 같이 폐기할 함수들을 실행합니다.
+  @override
+  void dispose() {
+    videoPlayerController?.removeListener(videoControllerListener);
+    super.dispose();
   }
 
   @override
@@ -92,17 +111,17 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   CustomIconButton(
-                    onPressed: () {},
+                    onPressed: onReversePressed,
                     iconData: Icons.rotate_left,
                   ),
                   CustomIconButton(
-                    onPressed: () {},
+                    onPressed: onPlayPressed,
                     iconData: videoPlayerController!.value.isPlaying
                         ? Icons.pause
                         : Icons.play_arrow,
                   ),
                   CustomIconButton(
-                    onPressed: () {},
+                    onPressed: onForwardPressed,
                     iconData: Icons.rotate_right,
                   ),
                 ],
@@ -110,5 +129,45 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
             )
           ],
         ));
+  }
+
+  void onReversePressed() {
+    // 되감기 버튼 눌렀을 때 실행할 함수
+    final currentPosition =
+        videoPlayerController!.value.position; // 현재 실행 중인 위치
+
+    Duration position = Duration(); // 0초로 싱행 위치 초기화
+
+    if (currentPosition.inSeconds > 3) {
+      // 현재 실행 위치가 3초보다 길 때만 3초 빼기
+      position = currentPosition - Duration(seconds: 3);
+    }
+
+    videoPlayerController!.seekTo(position);
+  }
+
+  void onForwardPressed() {
+    // 앞으로 감기 버튼 눌렀을 때 실행할 함수
+    final maxPosition = videoPlayerController!.value.duration; // 동영상 길이
+    final currentPosition = videoPlayerController!.value.position;
+
+    Duration position = maxPosition; // 동영상 길이로 실행 위치 초기화
+
+    // 동영상 길이에서 3초를 뺀 값보다 현재 위치가 짧을 때만 3초 더하기
+    if ((maxPosition - Duration(seconds: 3)).inSeconds >
+        currentPosition.inSeconds) {
+      position = currentPosition + Duration(seconds: 3);
+    }
+
+    videoPlayerController!.seekTo(position);
+  }
+
+  // 재생 버튼을 눌렀을 때 실행할 함수
+  void onPlayPressed() {
+    if (videoPlayerController!.value.isPlaying) {
+      videoPlayerController!.pause();
+    } else {
+      videoPlayerController!.play();
+    }
   }
 }
