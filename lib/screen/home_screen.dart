91816511638dfad4a4de514efbce95d087ea7a10
kite-torder <jjy5@torder.com>
2024-03-08 10:41:28 +0900

    feat: 동영상 존재 여부에 따른 함수 분기처리

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  XFile? video; // 동영상을 저장할 변수
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      backgroundColor: Colors.black,
+
+      // 동영상이 선택됐을 때와 선택 안 됐을 때 보여줄 위젯
+      body: video == null ? renderEmpty() : renderVideo(),
     );
+  }
+
+  // TODO 구현 예정
+  Widget renderEmpty() {
+    // 동영상 선택 전 보여줄 위젯
+    return Container();
+  }
+
+  // TODO 구현 예정
+  Widget renderVideo() {
+    // 동영상 선택 후 보여줄 위젯
+    return Container();
   }
 }
