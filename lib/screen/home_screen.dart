08950ac08f5a48d1a8a3ec29a7a676cd003e7905
kite-torder <jjy5@torder.com>
2024-03-08 14:30:44 +0900

    refactor: 홈 함수 리팩토링

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -14,50 +14,43 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  XFile? video; // 동영상을 저장할 변수
+  XFile? video;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-
-      // 동영상이 선택됐을 때와 선택 안 됐을 때 보여줄 위젯
       body: video == null ? renderEmpty() : renderVideo(),
     );
   }
 
   Widget renderEmpty() {
-    // 동영상 선택 전 보여줄 위젯
     return Container(
-      width: MediaQuery.of(context).size.width, // 최대 너비로 늘려주기
+      width: MediaQuery.of(context).size.width,
       decoration: getBoxDecoration(),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center, // 가운데 정렬
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           _Logo(
             onTap: onNewVideoPressed,
-          ), // 로고 탭하면 실행하는 함수
-          SizedBox(height: 30.0),
-          _AppName(), // 앱 이름
+          ),
+          const SizedBox(height: 30.0),
+          const _AppName(), // 앱 이름
         ],
       ),
     );
   }
 
   BoxDecoration getBoxDecoration() {
-    return BoxDecoration(
+    return const BoxDecoration(
       gradient: LinearGradient(
         begin: Alignment.topCenter,
         end: Alignment.bottomCenter,
-        colors: [
-          AppColors.color_003973,
-          AppColors.color_e5e5be
-        ],
+        colors: [AppColors.color_003973, AppColors.color_e5e5be],
       ),
     );
   }
 
-  // 이미지 선택하는 기능을 구현한 함수
   void onNewVideoPressed() async {
     final video = await ImagePicker().pickVideo(
       source: ImageSource.gallery,
@@ -74,10 +67,9 @@ class _HomeScreenState extends State<HomeScreen> {
     // 동영상 선택 후 보여줄 위젯
     return Center(
       child: CustomVideoPlayer(
-          video: video!,
-          onNewVideoPressed: onNewVideoPressed,
-      )
-      ,
+        video: video!,
+        onNewVideoPressed: onNewVideoPressed,
+      ),
     );
   }
 }
@@ -88,8 +80,8 @@ class _Logo extends StatelessWidget {
 
   const _Logo({
     required this.onTap,
-    Key? key,
-  }) : super(key: key);
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -112,7 +104,7 @@ class _AppName extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Row(
-      mainAxisAlignment: MainAxisAlignment.center, // 글자 가운데 정렬
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(
           Strings.VIDEO,
