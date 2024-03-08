38a32f33d4b91835061dd33a192eddd864e54030
kite-torder <jjy5@torder.com>
2024-03-08 09:57:22 +0900

    feat: img, video 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/screen/home_screen.dart';
 
 void main() {
-  // runApp(const MyApp());
+  runApp(const MaterialApp(home: HomeScreen()));
 }
